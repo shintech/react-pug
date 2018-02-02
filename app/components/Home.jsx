a2a322703ddf33199a75ccda3ffd7b1f7b0b94a7
Michael Prather <mprather1@gmail.com>
2018-02-02 07:15:59 +0000

    update

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
 export default class Home extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      home: []
+    }
+  }
+
+  componentDidMount () {
+    return fetch('/api/home')
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        home: json.body
+      })
+    })
+  }
+
   render () {
     return pug`
       div
         h1 Home
+        h4 ${this.state.home}
     `
   }
 }
--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default class Navigation extends React.Component {
   constructor (props) {
     super(props)
-    this.handleClick = this.handleClick.bind(this)
 
     this.state = {
       activeClassName: `#${props.location.pathname}`
@@ -16,21 +15,15 @@ export default class Navigation extends React.Component {
     }
   }
 
-  handleClick (e) {
-    this.setState({
-      activeClassName: e.target.dataset.path
-    })
-  }
-
   render () {
     return pug`
       nav
         li
-          a(onClick=${this.handleClick} data-path='/' className=${this.state.activeClassName === '#/' ? 'active' : null} href='#/') home
+          a(className=${this.state.activeClassName === '#/' ? 'active' : null} href='#/') home
         li
-          a(onClick=${this.handleClick} data-path='/page' className=${this.state.activeClassName === '#/page' ? 'active' : null} href='#/page') page 
+          a(className=${this.state.activeClassName === '#/page' ? 'active' : null} href='#/page') page 
         li
-          a(onClick=${this.handleClick} data-path='/about' className=${this.state.activeClassName === '#/about' ? 'active' : null} href='#/about') about               
+          a(className=${this.state.activeClassName === '#/about' ? 'active' : null} href='#/about') about               
     `
   }
 }
