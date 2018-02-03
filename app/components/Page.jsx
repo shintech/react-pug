9cc51dfdf7465f1b1b33b2f703aab7079ddb620f
Michael Prather <mprather1@gmail.com>
2018-02-03 08:55:29 +0000

    updated a little

--- app/components/Page.jsx
+++ app/components/Page.jsx
@@ -27,21 +27,23 @@ export default class Home extends React.Component {
 
   render () {
     return pug`
-      table
-        thead
-          tr
-            th.table-header Name
-            th.table-header Attribute
-            th.table-header Created At
-        tbody
-          ${this.state.models.map(model =>
-            pug`
-              tr(key=${model.id})
-                td ${model.name}
-                td ${model.attribute}
-                td ${model.created_at}
-              `
-          )}
+      div
+        h1 Example
+        table
+          thead
+            tr
+              th.table-header Name
+              th.table-header Attribute
+              th.table-header Created At
+          tbody
+            ${this.state.models.map(model =>
+              pug`
+                tr(key=${model.id})
+                  td ${model.name}
+                  td ${model.attribute}
+                  td ${model.created_at}
+                `
+            )}
     `
   }
 }
